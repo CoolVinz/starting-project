1849f244c2b7cc12227f3b8a47774eb70c6d640d
Paweenwat srisuk <paweenwat@banjan.ac.th>
2024-03-29 21:25:55 +0700

    try deploy

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ export default function Home() {
   console.log("...executing new");
   return (
     <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Get Start!</h1>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Get Start! ON Production
+      </h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <Link href={"/meals"}>Meals</Link>
